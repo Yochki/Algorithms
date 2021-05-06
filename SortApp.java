454f24b306610cd347587dc5cf98578560db8e69
Yochki <71455712+Yochki@users.noreply.github.com>
2021-05-06 21:09:28 +0300

    Add files via upload

---
+++ SortApp.java
@@ -0,0 +1,19 @@
+package Part_13;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class SortApp {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		frame.add(new SortControll());
+		frame.setTitle("Sorting");
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
